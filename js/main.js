9007acf31c87c843d27e57070860583b75a060c0
Kaushik-koshti <kaushikkoshti1511@gmail.com>
2024-10-15 19:26:40 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var inputBox = document.getElementById('name');
+
+function mass() {
+    var inputText = inputBox.value;
+    document.getElementById("ans").innerHTML = "Hello, " + "" +inputText +"!";
+}
+
+function clickChange() {
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    let rgbValue = "#" + randomColor;
+    document.getElementById("ans").style.color = rgbValue;
+}
